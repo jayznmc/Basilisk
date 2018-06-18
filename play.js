f887dceac57286b44983550c3afb5fff35062a03
Jason McGlasson <jam2478@my.open.ac.uk>
2018-06-18 23:34:01 +0100

    Moved index.html & play.js to GitTest folder

---
+++ play.js
@@ -0,0 +1,18 @@
+let size = 16;
+let board = " ";
+for (let i = 0; i < size; i++)
+{
+    for(let j = 0; j < size; j++)
+    {
+        if((i + j) % 2 == 0)
+        {
+            board += " ";
+        }
+        else
+        {
+            board += "#";
+        }
+    }
+    board += "\n";
+}
+console.log(board);
